8dbe96fb77026dbcabfb587be9d469f97e830652
Abusolay <sefat01625@gmail.com>
2025-09-14 19:07:19 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ export default function Navbar() {
   return (
     <div>
       <h1>i am Navbar </h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum necessitatibus quia numquam blanditiis dolores quidem?</p>
     </div>
   )
 }
