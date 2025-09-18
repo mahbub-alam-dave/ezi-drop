66ee7c85ea6cdf3572679cc2612c96a4f533a464
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-09-18 12:43:43 +0600

    register page and register form added

--- src/app/(site)/(authentication)/login/page.jsx
+++ src/app/(site)/(authentication)/login/page.jsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
     <div className="grid grid-cols-1 justify-between items-center gap-6 max-w-2xl mx-auto p-4 sm:px-6 lg:px-8 my-16">
       <div className="background-color rounded-2xl shadow-lg p-6 sm:p-10 lg:p-16">
-        <h2 className="text-2xl md:text-3xl font-bold mb-10 primary-text-color text-center">
+        <h2 className="text-2xl md:text-3xl font-bold mb-10 text-color text-center">
           Login
         </h2>
         <LoginForm />
--- src/components/authenticationComponents/RegisterForm.jsx
+++ src/components/authenticationComponents/RegisterForm.jsx
@@ -4,15 +4,27 @@ import Link from "next/link";
 import SocialLogin from "./SocialLogin";
 
 
-const LoginForm = () => {
 
 
-    const handleLogin = () => {
+const RegisterForm = () => {
+
+    const handleRegisterForm = () => {
 
     }
 
   return (
-    <form onSubmit={handleLogin} className="flex flex-col gap-4">
+    <form onSubmit={handleRegisterForm} className="flex flex-col gap-4">
+      <div>
+        <label htmlFor="name" className="">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          className="input-style w-full mt-2"
+          placeholder="Enter name"
+        />
+      </div>
       <div>
         <label htmlFor="email" className="">
           Email
@@ -39,18 +51,18 @@ const LoginForm = () => {
         type="submit"
         className="bg-[var(--color-secondary)] dark:bg-[var(--color-secondary-dark)] px-6 py-3 cursor-pointer rounded-full mt-4 w-full text-white font-medium text-lg"
       >
-        Login
+        Register
       </button>
       <span className="mt-6 text-center">Or Sign Up with</span>
       <SocialLogin />
       <span className="text-center">
-        Don't have an account?{" "}
-        <Link href={"/register"} className="text-[var(--color-primary)] dark:text-[var(--color-primary-dark)]">
-          register
+        Already have an account?{" "}
+        <Link href={"/login"} className="text-[var(--color-primary)] dark:text-[var(--color-primary-dark)]">
+          Login
         </Link>
       </span>
     </form>
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
