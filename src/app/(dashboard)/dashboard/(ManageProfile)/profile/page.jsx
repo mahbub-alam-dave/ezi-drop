daed2d9d578a33d50b0d6fba5f8cce7143af5820
Abdul Halim <abdulahlim1100@gmail.com>
2025-09-25 15:22:49 +0600

    show all role profile page in route

--- src/app/(dashboard)/dashboard/(ManageProfile)/profile/page.jsx
+++ src/app/(dashboard)/dashboard/(ManageProfile)/profile/page.jsx
@@ -4,6 +4,8 @@ export default function Page() {
   return (
     <div className="p-8">
       <ManageProfile role="rider" />
+      <ManageProfile role="user" />
+      <ManageProfile role="admin" />
       {/* role="rider" বা role="user" ও দিতে পারো */}
     </div>
   );
