6dba3e95a05871fe39b7c351c2f57486b363dad4
Abdul Halim <abdulahlim1100@gmail.com>
2025-09-25 15:15:46 +0600

    added edit profile functionality and inserted rider fake data

--- src/app/(dashboard)/dashboard/(ManageProfile)/profile/page.jsx
+++ src/app/(dashboard)/dashboard/(ManageProfile)/profile/page.jsx
@@ -3,7 +3,7 @@ import ManageProfile from "@/components/ManageProfile/ManageProfile";
 export default function Page() {
   return (
     <div className="p-8">
-      <ManageProfile role="admin" />
+      <ManageProfile role="rider" />
       {/* role="rider" বা role="user" ও দিতে পারো */}
     </div>
   );
