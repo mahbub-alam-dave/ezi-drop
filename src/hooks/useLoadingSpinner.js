213b6621d762581fed8c5a9693a7fb796cebede8
Abusolay <sefat01625@gmail.com>
2025-10-04 21:36:43 +0600

    use loading spinner use

--- src/hooks/useLoadingSpinner.js
+++ src/hooks/useLoadingSpinner.js
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 
 export default function useLoadingSpinner(message = "Loading...") {
   return useMemo(() => {
+    
     return (
       <div className="flex flex-col items-center justify-center h-[70vh] space-y-3">
         <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
