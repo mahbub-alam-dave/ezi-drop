554d583e1071cdb26a414e5b58f687f9cb5f037c
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-10-04 12:23:08 +0600

    added resulation page for agent admin, ticket status change functionaly and modified ui components

---
+++ src/app/(dashboard)/dashboard/(admin)/agent-resulation-center/page.jsx
@@ -0,0 +1,12 @@
+import SupportsTicket from '@/components/dashboardComponents/supportAgent(admin)/SupportsTicket';
+import React from 'react';
+
+const ResulationPage = () => {
+    return (
+        <div>
+            <SupportsTicket />
+        </div>
+    );
+};
+
+export default ResulationPage;
