1fcbc9042a38ffc37df5046465c68cf0c3f455b1
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-10-06 19:19:38 +0600

    added skeleton loader for site pages

---
+++ src/app/(site)/loading/page.js
@@ -0,0 +1,12 @@
+import SiteSkeleton from '@/components/loaders/skeletons/SiteSkeleton';
+import React from 'react';
+
+const SiteLoading = () => {
+    return (
+        <div>
+            <SiteSkeleton />
+        </div>
+    );
+};
+
+export default SiteLoading;
